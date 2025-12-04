c416c2a946883c4fe3635d3d68175365cc8dbe37
daniel <falcondaniel37@gmail.com>
2025-12-04 13:43:05 -0300

    se corrigieron inputs

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -3,18 +3,19 @@
  * COMPONENTE BASE DE INPUT
  * =====================================================
  * Componente centralizado para inputs que usa el sistema global de controles.
- * 
+ *
  * USO:
  *   <Input type="email" placeholder="Correo" />
  *   <Input type="password" size="sm" />
  *   <Input type="text" size="lg" icon={Mail} />
- * 
+ *
  * PROPS:
  *   - type: Tipo de input (text, email, password, etc.)
  *   - size: 'sm' | 'default' | 'lg' (default: 'default')
  *   - icon: Componente de icono (opcional, se posiciona a la izquierda)
  *   - iconRight: Componente de icono a la derecha (opcional)
  *   - className: Clases adicionales
+ *   - style: Estilos en linea adicionales
  *   - ...props: Props adicionales del input nativo
  * =====================================================
  */
@@ -29,56 +30,60 @@ const inputSizeClasses = {
   lg: 'input-lg',
 };
 
-const Input = React.forwardRef(({ 
+const Input = React.forwardRef(({
   type = 'text',
   size = 'default',
   icon,
   iconRight,
   className,
-  ...props 
+  style,
+  ...props
 }, ref) => {
   const sizeClass = inputSizeClasses[size] || '';
   const hasLeftIcon = !!icon;
   const hasRightIcon = !!iconRight;
 
-  // Calcular padding izquierdo según tamaño
-  const leftPadding = hasLeftIcon 
-    ? size === 'sm' ? 'pl-9' : size === 'lg' ? 'pl-12' : 'pl-10'
-    : '';
-  
-  // Calcular padding derecho según tamaño
-  const rightPadding = hasRightIcon
-    ? size === 'sm' ? 'pr-9' : size === 'lg' ? 'pr-12' : 'pr-10'
-    : '';
+  // Padding calculado con tokens para evitar solapamiento de iconos y texto
+  const paddingWithIcon = {
+    paddingLeft: hasLeftIcon
+      ? `calc(var(--control-padding-x) + var(--icon-size-md) + 14px)`
+      : undefined,
+    paddingRight: hasRightIcon
+      ? `calc(var(--control-padding-x) + var(--icon-size-md) + 14px)`
+      : undefined,
+  };
 
   return (
     <div className="relative w-full">
       {hasLeftIcon && (
-        <div className={cn(
-          "absolute top-1/2 -translate-y-1/2 z-10 pointer-events-none",
-          size === 'sm' ? 'left-2.5' : size === 'lg' ? 'left-4' : 'left-3'
-        )}>
+        <div
+          className={cn(
+            'absolute top-1/2 -translate-y-1/2 z-10 pointer-events-none',
+            size === 'sm' ? 'left-3' : size === 'lg' ? 'left-4' : 'left-3'
+          )}
+        >
           <Icon component={icon} size="xs" color="muted" />
         </div>
       )}
-      
+
       <input
         type={type}
         ref={ref}
         className={cn(
           sizeClass,
-          leftPadding,
-          rightPadding,
           className
         )}
+        style={{ ...paddingWithIcon, ...style }}
         {...props}
       />
-      
+
       {hasRightIcon && (
-        <div className={cn(
-          "absolute top-1/2 -translate-y-1/2 z-10",
-          size === 'sm' ? 'right-2.5' : size === 'lg' ? 'right-4' : 'right-3'
-        )}>
+        <div
+          className={cn(
+            'absolute top-1/2 -translate-y-1/2 z-10',
+            size === 'sm' ? 'right-2.5' : size === 'lg' ? 'right-4' : 'right-3'
+          )}
+        >
           {typeof iconRight === 'function' ? (
             <Icon component={iconRight} size="xs" color="muted" />
           ) : (
@@ -93,4 +98,3 @@ const Input = React.forwardRef(({
 Input.displayName = 'Input';
 
 export { Input };
-
